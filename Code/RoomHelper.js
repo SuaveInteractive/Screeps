0ceed11ecab15e6fded6102f8f5b3817bada35bf
Daniel Jones <daniel.lachlan.jones@gmail.com>
2016-07-11 20:46:05 +1000

    Exteneded the RoomHelper to remember which rooms have already been parsed.

--- Code/RoomHelper.js
+++ Code/RoomHelper.js
@@ -1,17 +1,35 @@
 module.exports = {
 
-    ParseRoom: function (room) {
+    ParseRoom: function (room)
+    {
+        if (HasRoomBeenParsed(room))
+            return
+
         console.log("What up")
 
         var sourcesActive = room.find(FIND_SOURCES_ACTIVE)
-        for (var i in sourcesActive) {
+        for (var i in sourcesActive)
+        {
             console.log("ACTIVE SOURCE" + i)
         }
 
         var sources = room.find(FIND_SOURCES_ACTIVE)
-        for (var i in sources) {
+        for (var i in sources)
+        {
             console.log("ACTIVE SOURCE" + i)
         }
+
+        Memory.ParsedRooms[room].Parsed = true
+    },
+
+    HasRoomBeenParsed: function(room)
+    {
+        for (var i in Memory.ParsedRooms)
+        {
+            if (i == room)
+                return room.Parsed ? room.Parsed : false
+        }
+        return false
     }
 };
 
