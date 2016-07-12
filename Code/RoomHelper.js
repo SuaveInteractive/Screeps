082305c7cf00c1482f646313cca0d31c3223456a
Daniel Jones <daniel.lachlan.jones@gmail.com>
2016-07-12 18:58:55 +1000

    More work on the RoomHelper

--- Code/RoomHelper.js
+++ Code/RoomHelper.js
@@ -2,28 +2,65 @@ Memory.ParsedRooms = {}
 
 module.exports = {
 
-    ParseRoom: function (room) {
+    ParseRoom: function (room)
+    {
         if (this.HasRoomBeenParsed(room))
             return
 
-        console.log("What up")
+        Memory.ParsedRooms[room] = {};
 
-        var sourcesActive = room.find(FIND_SOURCES)
+        Memory.ParsedRooms[room].Sources = {};
+        var sources = room.find(FIND_SOURCES)
+        _.forEach(sources, function (source)
+        {
+            var sourcePos = source.pos
+            var harvesterPositions = []
+           // console.log((sourcePos.y - 1) + " - " + (sourcePos.x - 1) + " - " + (sourcePos.y + 1) + " - " + (sourcePos.x + 1))
+
+            var area = room.lookForAtArea(LOOK_TERRAIN, sourcePos.y - 1, sourcePos.x - 1, sourcePos.y + 1, sourcePos.x + 1, false)
+
+            _.each(area, function (col, y)
+            {
+                _.each(col, function (cell, x)
+                {
+                    for (i = 0; i < cell.length; i++)
+                    {
+                        console.log(i + " pos " + x + " " + y + " cell.length: " + cell.length, " cell: " + cell);
+
+                        if (cell == "plain")
+                        {
+                            harvesterPositions.push[cell]
+                        }
+                    }
+                });
+            });
+
+            // console.log(result.length)
+            //for (var i=1; result.length; i++)
+            //{
+            //  console.log(result[i])
+            //}
+
+            Memory.ParsedRooms[room].Sources[source] =
+                {
+                    SourcePos: sourcePos,
+                    HarvesterPositions: harvesterPositions
+                }
+        });
+
+        var sourcesActive = room.find(FIND_SOURCES_ACTIVE)
         for (var i in sourcesActive) {
-            console.log("SOURCE" + i)
-        }
-
-        var sources = room.find(FIND_SOURCES_ACTIVE)
-        for (var i in sources) {
             console.log("SOURCE ACTIVE" + i)
         }
 
-        Memory.ParsedRooms[room] = {};
-        Memory.ParsedRooms[room].Parsed = true;
+
+        // Memory.ParsedRooms[room].Parsed = true;
     },
 
-    HasRoomBeenParsed: function (room) {
-        for (var i in Memory.ParsedRooms) {
+    HasRoomBeenParsed: function (room)
+    {
+        for (var i in Memory.ParsedRooms)
+        {
             console.log(i + " -- " + room)
             if (i == room)
                 return Memory.ParsedRooms[room].Parsed;
